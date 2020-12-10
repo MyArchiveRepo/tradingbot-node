339ec630c17dcb9acb9ea34fdc924bf72dd6bc53
Aldo Malerba <aldomalerba.dev@gmail.com>
2020-12-10 19:04:00 +0100

    order status

--- src/services/orderStatus.js
+++ src/services/orderStatus.js
@@ -1,6 +1,6 @@
 module.exports = {
-    SELL_REPAY,
-    SELL_SHORT,
-    BUY_REPAY,
-    BUY_LONG
+    SELL_REPAY: "SELL_REPAY",
+    SELL_SHORT: "SELL_SHORT",
+    BUY_REPAY: "BUY_REPAY",
+    BUY_LONG: "BUY_LONG"
 }
